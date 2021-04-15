574a33cf2e8044064829a4664e8424f1c7e24df9
minus3theta <yone_j_synthesis@yahoo.co.jp>
2021-04-16 01:36:54 +0900

    Implement printk

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -13,6 +13,22 @@ void operator delete(void *) noexcept {}
 char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
 PixelWriter *pixel_writer;
 
+char console_buf[sizeof(Console)];
+Console *console;
+
+int printk(const char *format, ...) {
+  va_list ap;
+  int result;
+  char s[1024];
+
+  va_start(ap, format);
+  result = vsprintf(s, format, ap);
+  va_end(ap);
+
+  console->PutString(s);
+  return result;
+}
+
 extern "C" void KernelMain(const FrameBufferConfig &frame_buffer_config) {
   switch (frame_buffer_config.pixel_format) {
   case kPixelRGBResv8BitPerColor:
@@ -30,17 +46,11 @@ extern "C" void KernelMain(const FrameBufferConfig &frame_buffer_config) {
       pixel_writer->Write(x, y, {255, 255, 255});
     }
   }
-  for (int x = 0; x < 200; ++x) {
-    for (int y = 0; y < 100; ++y) {
-      pixel_writer->Write(100 + x, 100 + y, {0, 255, 0});
-    }
-  }
 
-  Console console(*pixel_writer, {0, 0, 0}, {255, 255, 255});
-  char buf[128];
+  console =
+      new (console_buf) Console(*pixel_writer, {0, 0, 0}, {255, 255, 255});
   for (int i = 0; i < 30; ++i) {
-    sprintf(buf, "line %d\n", i);
-    console.PutString(buf);
+    printk("printk: %d\n", i);
   }
 
   while (1) {
