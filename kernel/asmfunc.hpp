4a747830ab44cf849ea237a041324f74d0296305
minus3theta <yone_j_synthesis@yahoo.co.jp>
2021-04-30 00:20:11 +0900

    Set identity page table

--- kernel/asmfunc.hpp
+++ kernel/asmfunc.hpp
@@ -7,4 +7,8 @@ void IoOut32(uint16_t addr, uint32_t data);
 uint32_t IoIn32(uint16_t addr);
 uint16_t GetCS();
 void LoadIDT(uint16_t limit, uint64_t offset);
+void LoadGDT(uint16_t limit, uint64_t offset);
+void SetDSAll(uint16_t value);
+void SetCSSS(uint16_t cs, uint16_t ss);
+void SetCR3(uint64_t value);
 }
