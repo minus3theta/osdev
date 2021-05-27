d4c42c539d66a32d4dca53a745db12c494c3e65d
minus3theta <yone_j_synthesis@yahoo.co.jp>
2021-05-28 02:12:23 +0900

    Implement task manager

--- kernel/task.cpp
+++ kernel/task.cpp
@@ -1,14 +1,13 @@
 #include "task.hpp"
 
+#include <cstddef>
+
 #include "asmfunc.hpp"
+#include "segment.hpp"
 #include "timer.hpp"
 
-namespace {
-TaskContext *current_task;
-}
-
 void InitializeTask() {
-  current_task = &task_a_ctx;
+  task_manager = new TaskManager;
 
   __asm__("cli");
   timer_manager->AddTimer(
@@ -16,8 +15,46 @@ void InitializeTask() {
   __asm__("sti");
 }
 
-void SwitchTask() {
-  TaskContext *old_current_task = current_task;
-  current_task = current_task == &task_a_ctx ? &task_b_ctx : &task_a_ctx;
-  SwitchContext(current_task, old_current_task);
+Task::Task(uint64_t id) : id(id) {}
+
+Task &Task::InitContext(TaskFunc *f, int64_t data) {
+  const size_t stack_size = kDefaultStackBytes / sizeof(stack[0]);
+  stack.resize(stack_size);
+  uint64_t stack_end = reinterpret_cast<uint64_t>(&stack[stack_size]);
+
+  memset(&context, 0, sizeof(context));
+  context.cr3 = GetCR3();
+  context.rflags = 0x202;
+  context.cs = kKernelCS;
+  context.ss = kKernelSS;
+  context.rsp = (stack_end & ~0xflu) - 8;
+  context.rip = reinterpret_cast<uint64_t>(f);
+  context.rdi = id;
+  context.rsi = data;
+
+  *reinterpret_cast<uint32_t *>(&context.fxsave_area[24]) = 0x1f80;
+
+  return *this;
+}
+
+TaskContext &Task::Context() { return context; }
+
+TaskManager::TaskManager() { NewTask(); }
+
+Task &TaskManager::NewTask() {
+  ++latest_id;
+  return *tasks.emplace_back(new Task{latest_id});
+}
+
+void TaskManager::SwitchTask() {
+  size_t next_task_index = current_task_index + 1;
+  if (next_task_index >= tasks.size()) {
+    next_task_index = 0;
+  }
+
+  Task &current_task = *tasks[current_task_index];
+  Task &next_task = *tasks[next_task_index];
+  current_task_index = next_task_index;
+
+  SwitchContext(&next_task.Context(), &current_task.Context());
 }
--- kernel/task.hpp
+++ kernel/task.hpp
@@ -2,6 +2,8 @@
 
 #include <array>
 #include <cstdint>
+#include <memory>
+#include <vector>
 
 struct TaskContext {
   uint64_t cr3, rip, rflags, reserved1;
@@ -13,5 +15,33 @@ struct TaskContext {
 
 alignas(16) inline TaskContext task_b_ctx, task_a_ctx;
 
-void SwitchTask();
 void InitializeTask();
+
+using TaskFunc = void(uint64_t, int64_t);
+
+class Task {
+public:
+  static const size_t kDefaultStackBytes = 4096;
+  Task(uint64_t id);
+  Task &InitContext(TaskFunc *f, int64_t data);
+  TaskContext &Context();
+
+private:
+  uint64_t id;
+  std::vector<uint64_t> stack;
+  alignas(16) TaskContext context;
+};
+
+class TaskManager {
+public:
+  TaskManager();
+  Task &NewTask();
+  void SwitchTask();
+
+private:
+  std::vector<std::unique_ptr<Task>> tasks;
+  uint64_t latest_id{0};
+  size_t current_task_index{0};
+};
+
+inline TaskManager *task_manager;
