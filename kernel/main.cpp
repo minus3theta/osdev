2b68fe469065dbff7228220e1f40d5c686c47b82
minus3theta <yone_j_synthesis@yahoo.co.jp>
2021-07-26 13:58:53 +0900

    Spawn terminal by F2

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -212,6 +212,9 @@ KernelMainNewStack(const FrameBufferConfig &frame_buffer_config_ref,
         if (msg->arg.keyboard.press) {
           InputTextWindow(msg->arg.keyboard.ascii);
         }
+      } else if (msg->arg.keyboard.press &&
+                 msg->arg.keyboard.keycode == 59 /* F2 */) {
+        task_manager->NewTask().InitContext(TaskTerminal, 0).Wakeup();
       } else {
         __asm__("cli");
         auto task_it = layer_task_map->find(act);
