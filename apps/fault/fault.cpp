a91680cf4eed147150f065018e3daf369ad1399a
minus3theta <yone_j_synthesis@yahoo.co.jp>
2021-07-26 15:46:17 +0900

    Add apps/fault

---
+++ apps/fault/fault.cpp
@@ -0,0 +1,26 @@
+#include "../syscall.h"
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+extern "C" void main(int argc, char **argv) {
+  const char *cmd = "hlt";
+  if (argc >= 2) {
+    cmd = argv[1];
+  }
+
+  if (strcmp(cmd, "hlt") == 0) {
+    __asm__("hlt");
+  } else if (strcmp(cmd, "wr_kernel") == 0) {
+    int *p = reinterpret_cast<int *>(0x100);
+    *p = 42;
+  } else if (strcmp(cmd, "wr_app") == 0) {
+    int *p = reinterpret_cast<int *>(0xffff'8000'ffff'0000);
+    *p = 123;
+  } else if (strcmp(cmd, "zero") == 0) {
+    volatile int z = 0;
+    printf("100/%d = %d\n", z, 100 / z);
+  }
+
+  exit(0);
+}
